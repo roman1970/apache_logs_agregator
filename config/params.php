789660295734ce71a4b99932ba676589dc108cb7
roman1970 <r0man4ernyshev@gmail.com>
2016-11-14 13:41:52 +0700

    edit Readme

--- config/params.php
+++ config/params.php
@@ -1,9 +1,6 @@
 <?php
 
 return [
-    //'adminEmail' => 'admin@example.com',
-    //'supportEmail' => '',
-    //'user.passwordResetTokenExpire' => 3600,
     'log_files' => [
         'apache' => '/var/log/apache2/access.log',
         'nginx' => '/var/log/nginx/access.log'
